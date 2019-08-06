48cc3589de2c23c6d38c00a8ad410b67429976f4
Tony Lomax <antl.lomax@gmail.com>
2019-08-06 21:02:18 +0100

    Function/get-repo-url Iterate through the response object and get homepage urls

--- index.js
+++ index.js
@@ -4,6 +4,9 @@ xhr.onreadystatechange = function() {
   if (xhr.readyState == 4 && xhr.status == 200) {
     let response = JSON.parse(xhr.responseText);
     console.log(response);
+    response.forEach(e => {
+      console.log(e.homepage);
+    });
   }
 };
 xhr.open("GET", `https://api.github.com/users/tonylomax/repos`, true);
